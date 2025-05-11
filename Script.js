f74d3b5dafe41bb0c165f5e8b2c6e73b4ccca39d
Will Canterbury <canterburywill@gmail.com>
2025-05-10 17:04:59 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+// Example: Log a message to the browser console when the page loads
+console.log("Website loaded!");
+
+// Example: Add a simple interactive feature
+//const heading = document.querySelector('h1');
+//heading.addEventListener('mouseover', function () {
+    //heading.style.color = 'lightblue';
+//});
+
+heading.addEventListener('mouseout', function () {
+    heading.style.color = 'white'; // Change it back
+});
